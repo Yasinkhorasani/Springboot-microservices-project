4fafb7e7f97b3b9513840e6e545db4aee2fc1a3a
Yasin Khorasani <103818643+Yasinkhorasani@users.noreply.github.com>
2022-12-24 18:49:10 +0100

    Add files via upload

---
+++ order-service/src/test/java/com/programm/order/service/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programm.order.service.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
